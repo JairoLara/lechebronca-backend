18ba07ce5084959d90a6446b55a4df75b9703067
Jordix69 <laracisnerosjairo@gmail.com>
2025-06-16 22:35:00 -0600

    endpoint project  individual

--- app.js
+++ app.js
@@ -72,6 +72,17 @@ app.get('/images', async (req, res) => {
   }
 });
 
+app.get('/images/:id', async (req, res) => {
+  try {
+    const image = await Image.findByPk(req.params.id);
+    if (!image) {
+      return res.status(404).json({ error: 'Imagen no encontrada' });
+    }
+    res.json(image);
+  } catch (err) {
+    res.status(500).json({ error: 'Error interno del servidor' });
+  }
+});
 // Ruta para fotos
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
